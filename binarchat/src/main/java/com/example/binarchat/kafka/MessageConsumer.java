1da46f7ecde3d77b516cac88e4ea6eb185016320
birruby <ilyas@lua.co.id>
2023-11-27 21:41:31 +0700

    fix get topic name

--- binarchat/src/main/java/com/example/binarchat/kafka/MessageConsumer.java
+++ binarchat/src/main/java/com/example/binarchat/kafka/MessageConsumer.java
@@ -1,13 +1,15 @@
 package com.example.binarchat.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
 
     @KafkaListener(topics="binarchat", groupId = "binar-group-id")
-    public void sendMessage(String topic, String message){
+    public void listen(String message, @Header(KafkaHeaders.RECEIVED_TOPIC) String topic){
         System.out.println("Pesan diterima di topic"+ topic+": "+message);
     }
 }
